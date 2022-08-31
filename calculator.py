e4ba470a9bade5c985cf147b4123f5323b048e09
Lu Liu <luliu@Lus-MacBook-Pro.attlocal.net>
2022-08-30 22:16:23 -0500

    introducing floats

--- calculator.py
+++ calculator.py
@@ -8,7 +8,17 @@
 
 
 ### 2nd Approach
-x = int(input("What's x? "))
-y = int(input("What's y? "))
+# x = int(input("What's x? "))
+# y = int(input("What's y? "))
+#
+# print(x+y)
 
-print(x+y)
+### float
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+z = x / y
+# z = round(x/y, 2)   # x = 2, y = 3, round to the nearest 2 decimal places
+
+
+# print(f"{z:,}") # automatic formatting with commas
+print(f"{z:.2f}")   # using f string to print 2 decimal places
