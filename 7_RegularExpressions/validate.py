009625c12540cfc490687ea4a34c317c3c18601b
lul15 <lul15@users.noreply.github.com>
2022-11-02 22:09:26 -0500

    Update validate.py
    merge conflict

--- 7_RegularExpressions/validate.py
+++ 7_RegularExpressions/validate.py
@@ -1,12 +1,25 @@
 import re
 
 
-email = input("What's your email? ")
+email = input("What's your email? ").strip()
 
-
-if re.search(r"^[^@]+@[^@]+\.edu$", email): 
-    print("Valid") 
-else: 
+if re.search(r".+@.+\.edu", email):
+    print("Valid")
+else:
     print("Invalid")
 
 
+
+# # 2nd iteration
+# username, domain = email.split("@")
+# if username and domain.endswith(".edu"):
+#     print("Valid")
+# else:
+#     print("Invalid")
+
+
+# # 1st iteration - rudimentary validation
+# if "@" in email and "." in email:
+#     print("Valid")
+# else:
+#     print("Invalid")
